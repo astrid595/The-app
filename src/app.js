9584000eb49f8e3123dd0ea61710c01d61a44a62
astrid595 <84007019+astrid595@users.noreply.github.com>
2021-11-15 10:52:51 +1100

    Editing current date

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 function displayTemperature (response) {
     console.log(response.data.main.temp);
+
     let temperatureElement = document.querySelector("#temperature");
     let cityeElement = document.querySelector("#city");
     let descriptionElement = document.querySelector ("#description");
@@ -8,6 +9,7 @@ function displayTemperature (response) {
     cityeElement.innerHTML=response.data.name;
     descriptionElement.innerHTML=response.data.weather[0].description;
 }
+
 let apiKey = "c8116d2b99630d13abef344e372457d3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey};
 axios.get(apiUrl).then(displayTemperature);
