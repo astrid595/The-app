8cbcc1ffa6cb4080e9979938f2997c9c582d1e0e
astrid595 <84007019+astrid595@users.noreply.github.com>
2021-11-15 16:14:22 +1100

    Update time

--- src/app.js
+++ src/app.js
@@ -1,34 +1,33 @@
 function formatDate(timestamp) {
-let date = new Date(timestamp);
-let hours = date.getHours();
-if (hours <10) {
-    hours = `0${minutes}`;
-}
+    let date = new Date (timestamp);
+    let hours = date.getHours();
+    if (hours <10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes <10) {
+        minutes = `0${minutes}`;
+    }
 
 
-let minutes = date.getMinutes();
-if (minutes <10) {
-    minutes = `0${minutes}`;
 
-}
-
-let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-let day = days[date.getDay()];
-return `${day}; ${hours} ${minutes}`;
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+    let day = days[date.getDay()];
+return `${day}`;
 }
 
 
 
 function displayTemperature(response){
-
+    console.log(response.data);
 
 
 let temperatureElement = document.querySelector("#temperature");
@@ -40,13 +39,11 @@ cityElement.innerHTML = response.data.name;
 let conditionElement = document.querySelector("#condition");
 conditionElement.innerHTML = response.data.weather[0].description;
 
-let dateElement = document.querySelector("date");
-dateElement.innerHTML = formatDate(response.data.dt*10000);
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatDate(response.data.dt*1000);
 
 
 }
-
-
 
 
 let apiKey = "c8116d2b99630d13abef344e372457d3";
