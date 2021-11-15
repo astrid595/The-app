b978e8d4c2ee1672557d74bf204433be5b21aa64
astrid595 <84007019+astrid595@users.noreply.github.com>
2021-11-15 14:43:05 +1100

    update time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours <10) {
+    hours = `0${minutes}`;
+}
+
+
+let minutes = date.getMinutes();
+if (minutes <10) {
+    minutes = `0${minutes}`;
+
+}
+
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+let day = days[date.getDay()];
+return `${day}; ${hours} ${minutes}`;
+}
+
+
+
 function displayTemperature(response){
 
     console.log(response.data);
@@ -11,6 +40,10 @@ cityElement.innerHTML = response.data.name;
 
 let conditionElement = document.querySelector("#condition");
 conditionElement.innerHTML = response.data.weather[0].description;
+
+let dateElement = document.querySelector("date");
+dateElement.innerHTML = formatDate(response.data.dt*10000);
+
 
 }
 
