95ac8a121f1e6fe95a9f25fc665ac64c3ae68d41
astrid595 <84007019+astrid595@users.noreply.github.com>
2021-11-15 14:20:30 +1100

    Setting current temperature

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
+function displayTemperature(response){
+
+    console.log(response.data);
+
+
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round (response.data.main.temp);
+
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML = response.data.name;
+
+let conditionElement = document.querySelector("#condition");
+conditionElement.innerHTML = response.data.weather[0].description;
+
+}
+
+
+
+
 let apiKey = "c8116d2b99630d13abef344e372457d3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey};
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=c8116d2b99630d13abef344e372457d3&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
