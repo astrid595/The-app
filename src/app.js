4d87f7fa08f25e1c0c23b742f5808dc4a7a2ad12
astrid595 <84007019+astrid595@users.noreply.github.com>
2021-11-15 11:52:37 +1100

    starting over

--- src/app.js
+++ src/app.js
@@ -1,15 +1,2 @@
-function displayTemperature (response) {
-    console.log(response.data.main.temp);
-
-    let temperatureElement = document.querySelector("#temperature");
-    let cityeElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector ("#description");
-    temperatureElement.innerHTML = Math.round;
-    (response.data.main.temp);
-    cityeElement.innerHTML=response.data.name;
-    descriptionElement.innerHTML=response.data.weather[0].description;
-}
-
 let apiKey = "c8116d2b99630d13abef344e372457d3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey};
-axios.get(apiUrl).then(displayTemperature);
