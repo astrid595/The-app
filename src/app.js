2503022634044e110d055f47c46d6e8e28c8d0b3
astrid595 <84007019+astrid595@users.noreply.github.com>
2021-11-15 16:31:14 +1100

    Added icon with API

--- src/app.js
+++ src/app.js
@@ -42,11 +42,15 @@ conditionElement.innerHTML = response.data.weather[0].description;
 let dateElement = document.querySelector("#date");
 dateElement.innerHTML = formatDate(response.data.dt*1000);
 
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
 
 
 let apiKey = "c8116d2b99630d13abef344e372457d3";
+let city = "Los Angeles";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=c8116d2b99630d13abef344e372457d3&units=metric";
 
 axios.get(apiUrl).then(displayTemperature);
