e347fb41b8d77b2571f57aadc9decfc71fd5e296
astrid595 <84007019+astrid595@users.noreply.github.com>
2021-11-25 14:06:11 +1100

    Display the forecast data using API

--- src/app.js
+++ src/app.js
@@ -22,35 +22,46 @@ function formatDate(timestamp) {
     return `${day}, ${hours}:${minutes}`;
   }
 
-  function displayForecast () {
+  function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue","Wed","Thu","Fri","Sat"];
+
+    return days[day];
+  }
+
+  function displayForecast (response) {
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
-    let days = ["Thu", "Fri", "Sat", "Sun"];
-    days.forEach(function(day)
+    forecast.forEach(function(forecastDay, index)
    {
+     if (index <6) {
     forecastHTML =   
-    forecastHTML +  
-    `
-    <div class="col-2">
-      <div class="weather-forecast-date">
-          ${day}
+      forecastHTML +  
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">
+            ${formatDay(forecastDay.dt)}
+        </div>
+        
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+        alt=""
+        width="20"
+        >
+        <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">
+                ${Math.round(forecastDay.temp.max)}°
+            </span>
+            <span class="weather-forecast-temperature-max">
+                ${Math.round(forecastDay.temp.min)}°
+            </span>
+        </div>
+        
       </div>
-      <img src="https://tse2.mm.bing.net/th?id=OIP.2MEM7UwySzRAjdqQKS5IwgHaHa&pid=Api" 
-      alt=""
-      width="20"
-      >
-      <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-min">
-              18°
-          </span>
-          <span class="weather-forecast-temperature-max">
-              12°
-          </span>
-      </div>
-      
-    </div>
-    `;
+      `;
+     }
    } )
     
    
@@ -58,6 +69,13 @@ function formatDate(timestamp) {
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 
+  }
+
+  function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "c8116d2b99630d13abef344e372457d3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
   }
   
   function displayTemperature(response) {
@@ -85,6 +103,9 @@ function formatDate(timestamp) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
+  
+      getForecast(response.data.coord)
+  
   }
   
   function search(city) {
@@ -104,4 +125,4 @@ function formatDate(timestamp) {
   form.addEventListener("submit", handleSubmit);
   
   search("London");
-  displayForecast();
+  
